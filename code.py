b499296bf14fdb5c216f4ea0b347e73699b4d3e4
Sp3lllz <136860095+sp3lllz@users.noreply.github.com>
2023-11-20 21:01:45 +0000

    Update code.py

--- code.py
+++ code.py
@@ -5,57 +5,25 @@ from adafruit_hid.keyboard import Keyboard
 import board
 import digitalio
 
+button_pins = [
+    board.GP0, board.GP1, board.GP2, board.GP3, board.GP4,
+    board.GP5, board.GP6, board.GP7, board.GP8, board.GP9,
+    board.GP10, board.GP11, board.GP12, board.GP13, board.GP14,
+    board.GP15, board.GP26, board.GP27, board.GP28, board.GP29
+]
 
-btn1_pin = board.GP9
-btn2_pin = board.GP8
-btn3_pin = board.GP7
-btn4_pin = board.GP19
-btn5_pin = board.GP20
-btn6_pin = board.GP21
+buttons = [digitalio.DigitalInOut(pin) for pin in button_pins]
 
-btn1 = digitalio.DigitalInOut(btn1_pin)
-btn1.direction = digitalio.Direction.INPUT
-btn1.pull = digitalio.Pull.DOWN
-
-btn2 = digitalio.DigitalInOut(btn2_pin)
-btn2.direction = digitalio.Direction.INPUT
-btn2.pull = digitalio.Pull.DOWN
-
-btn3 = digitalio.DigitalInOut(btn3_pin)
-btn3.direction = digitalio.Direction.INPUT
-btn3.pull = digitalio.Pull.DOWN
-
-btn4 = digitalio.DigitalInOut(btn4_pin)
-btn4.direction = digitalio.Direction.INPUT
-btn4.pull = digitalio.Pull.DOWN
-
-btn5 = digitalio.DigitalInOut(btn5_pin)
-btn5.direction = digitalio.Direction.INPUT
-btn5.pull = digitalio.Pull.DOWN
-
-btn6 = digitalio.DigitalInOut(btn6_pin)
-btn6.direction = digitalio.Direction.INPUT
-btn6.pull = digitalio.Pull.DOWN
+for button in buttons:
+    button.direction = digitalio.Direction.INPUT
+    button.pull = digitalio.Pull.DOWN
 
 keyboard = Keyboard(usb_hid.devices)
 
 while True:
-    if btn1.value:
-        keyboard.send(Keycode.CONTROL, Keycode.F7)
-        time.sleep(0.1)
-    if btn2.value:
-        keyboard.send(Keycode.CONTROL, Keycode.F8)
-        time.sleep(0.1)
-    if btn3.value:
-        keyboard.send(Keycode.CONTROL, Keycode.F9)
-        time.sleep(0.1)
-    if btn4.value:
-        keyboard.send(Keycode.CONTROL, Keycode.F10)
-        time.sleep(0.1)
-    if btn5.value:
-        keyboard.send(Keycode.CONTROL, Keycode.F11)
-        time.sleep(0.1)
-    if btn6.value:
-        keyboard.send(Keycode.CONTROL, Keycode.F12)
-        time.sleep(0.1)
+    for i, button in enumerate(buttons):
+        if button.value:
+            # Use i+1 as the Keycode, assuming you want to map buttons to F1-F20
+            keyboard.send(Keycode.F1 + i)
+            time.sleep(0.1)
     time.sleep(0.1)
